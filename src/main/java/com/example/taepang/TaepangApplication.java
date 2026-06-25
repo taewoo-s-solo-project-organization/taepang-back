bfe9bd0c7f2df22b53b4972acae30074f7718993
wootaepark <pakdal99@gmail.com>
2026-06-25 13:20:24 +0900

    init commit

---
+++ src/main/java/com/example/taepang/TaepangApplication.java
@@ -0,0 +1,13 @@
+package com.example.taepang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaepangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaepangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taepang/TaepangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taepang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaepangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
