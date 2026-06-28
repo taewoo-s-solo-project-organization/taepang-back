2137a4145dff024e7f1f1086e4e10301ad4a7cd9
wootaepark <pakdal99@gmail.com>
2026-06-28 13:43:42 +0900

    feat : user, seller 테이블 관계 설정 및 manager 테이블 구축

---
+++ src/main/java/com/example/taepang/domain/member/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.taepang.domain.member.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String username;
+	private String phoneNumber;
+	private String email;
+
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+}
